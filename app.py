0e552ec8808eb3209dcc48a132d61aa1e144409b
clarissafdz <clarissafernandezr@gmail.com>
2023-11-19 00:11:01 -0500

    new model

--- app.py
+++ app.py
@@ -1,52 +1,43 @@
-from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
-import tensorflow as tf
+# app.py
+from flask import Flask, request, jsonify, render_template
+from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 import io
-import os
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('asl_alphabet_model.h5')  # Ensure the model path is correct
 
+# Load the pre-trained model (make sure to provide the correct path and model name)
+model = load_model('final_asl_alphabet_model')
 
 @app.route('/')
 def index():
-    # Render your HTML page
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
-@app.route('/predict-asl', methods=['POST'])
-def predict_asl():
-    # Check if a valid image file was uploaded
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    
-    file = request.files['file']
+@app.route('/predict-frame', methods=['POST'])
+def predict_frame():
+    if 'frame' not in request.files:
+        return jsonify({'error': 'No frame part in the request'}), 400
+
+    file = request.files['frame']
     if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify({'error': 'No file selected for uploading'}), 400
 
     if file:
-        # Read the image and preprocess it for the model
+        # Read the image and preprocess it
         image = Image.open(io.BytesIO(file.read()))
         image = image.resize((200, 200))
-        image = np.expand_dims(image, axis=0)
-        image = np.array(image) / 255.0  # Normalize the image
-        
-        # Predict the class of the image
+        image = np.array(image)
+        if image.shape[-1] == 4:  # Check if image has an alpha channel
+            image = image[..., :3]  # Convert from RGBA to RGB
+        image = np.expand_dims(image, axis=0) / 255.0
+
+        # Make prediction
         prediction = model.predict(image)
-        predicted_class = np.argmax(prediction, axis=1)[0]  # Assuming class indices are labels
-        
-        # Convert the class index into the corresponding ASL letter
-        asl_letter = chr(predicted_class + 65)  # 65 is the ASCII for 'A'
+        predicted_class = np.argmax(prediction, axis=1)[0]
+        asl_letter = chr(predicted_class + 65)  # Assuming ASCII
 
         return jsonify({'prediction': asl_letter})
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
-
 if __name__ == '__main__':
-    # Define the PORT on which you want to run the server
-    PORT = os.environ.get('PORT', 5000)
-    # Start the Flask server
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(debug=True)
--- model_training.py
+++ model_training.py
@@ -1,19 +1,35 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
 # Load the annotations into a DataFrame
-annotations = pd.read_csv('American Sign Language Letters.v1-v1.tensorflow/train/_annotations.csv')
-# Assuming your CSV has 'filename' and 'class' columns
-# Adjust column names if necessary
+train_annotations = pd.read_csv('American Sign Language Letters.v1-v1.tensorflow/train/_annotations.csv')
+valid_annotations = pd.read_csv('American Sign Language Letters.v1-v1.tensorflow/valid/_annotations.csv')
 
-# Split data into training and validation sets
-train_df, valid_df = train_test_split(annotations, test_size=0.2, stratify=annotations['class'])
+# Calculate class weights to counter class imbalances
+class_weights = compute_class_weight(
+    class_weight='balanced',
+    classes=np.unique(train_annotations['class']),
+    y=train_annotations['class'].values
+)
+class_weights = dict(enumerate(class_weights))
 
-# Set up the ImageDataGenerator for data augmentation
-datagen = ImageDataGenerator(
-    rescale=1./255,
+# Image preprocessing function
+def preprocess_image(image):
+    image = tf.image.convert_image_dtype(image, tf.float32)  # Convert to float values in [0, 1]
+    return tf.image.resize(image, [200, 200])
+
+# Data augmentation for the training set
+train_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_image,
     rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
@@ -23,10 +39,13 @@ datagen = ImageDataGenerator(
     fill_mode='nearest'
 )
 
-# Create generators
-train_generator = datagen.flow_from_dataframe(
-    dataframe=train_df,
-    directory='American Sign Language Letters.v1-v1.tensorflow/train',  # Update this to the path of your images
+# Only rescaling for the validation set
+valid_datagen = ImageDataGenerator(preprocessing_function=preprocess_image)
+
+# Create data generators
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_annotations,
+    directory='American Sign Language Letters.v1-v1.tensorflow/train',
     x_col='filename',
     y_col='class',
     target_size=(200, 200),
@@ -34,9 +53,9 @@ train_generator = datagen.flow_from_dataframe(
     class_mode='categorical'
 )
 
-valid_generator = datagen.flow_from_dataframe(
-    dataframe=valid_df,
-    directory='American Sign Language Letters.v1-v1.tensorflow/train',  # Update this to the path of your images
+valid_generator = valid_datagen.flow_from_dataframe(
+    dataframe=valid_annotations,
+    directory='American Sign Language Letters.v1-v1.tensorflow/valid',
     x_col='filename',
     y_col='class',
     target_size=(200, 200),
@@ -44,28 +63,40 @@ valid_generator = datagen.flow_from_dataframe(
     class_mode='categorical'
 )
 
-# Define the model
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(len(train_generator.class_indices), activation='softmax')
+# Load the VGG16 model, pre-trained on ImageNet
+base_model = VGG16(input_shape=(200, 200, 3), include_top=False, weights='imagenet')
+base_model.trainable = False  # Freeze the VGG16 model
+
+# Add custom layers on top of VGG16
+model = Sequential([
+    base_model,
+    GlobalAveragePooling2D(),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(len(train_generator.class_indices), activation='softmax')
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Callbacks
+checkpoint = ModelCheckpoint('asl_alphabet_model_vgg16', save_best_only=True, verbose=1, monitor='val_accuracy', mode='max')
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6, verbose=1)
 
 # Train the model
 history = model.fit(
     train_generator,
     steps_per_epoch=len(train_generator),
+    epochs=50,
     validation_data=valid_generator,
     validation_steps=len(valid_generator),
-    epochs=25
+    class_weight=class_weights,
+    callbacks=[checkpoint, early_stopping, reduce_lr]
 )
 
-# Save the model
-model.save('asl_alphabet_model.h5')
+# Save the final model in TensorFlow SavedModel format
+model.save('final_asl_alphabet_model')
+
+# Replace 'path_to_your_train_annotations.csv', 'path_to_your_valid_annotations.csv', 
+# 'path_to_your_training_images', and 'path_to_your_validation_images' with the actual paths to your files.
